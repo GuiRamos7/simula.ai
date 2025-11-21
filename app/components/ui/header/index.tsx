00594f2a519654e8a9895aaae798c0c482922323
Guilherme Ramos <guioliveiraramos10@gmail.com>
2025-11-21 14:09:50 -0300

    changing component folder layer

---
+++ app/components/ui/header/index.tsx
@@ -0,0 +1,18 @@
+import { useTheme } from '@/app/providers/ThemeContext';
+import { ThemeSwitcher } from '../shadcn-io/theme-switcher';
+
+export const Header = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <header className="bg-background border-input w-container fixed top-0 right-0 left-0 w-full border-b-2 p-1">
+      <div className="m-auto flex w-5/6 items-center justify-between">
+        <h1 className="bold text-3xl">
+          Simula
+          <span className="ai bold text-5xl text-pink-500 italic">.aí</span>
+        </h1>
+        <ThemeSwitcher value={theme} onChange={(e) => toggleTheme(e)} />
+      </div>
+    </header>
+  );
+};
