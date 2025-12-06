f0b3e38c7e489a6dd071b7b13b6ec7fc9e884827
Guilherme Ramos <guioliveiraramos10@gmail.com>
2025-12-06 16:07:00 -0300

    fixing header link

--- app/components/ui/header/index.tsx
+++ app/components/ui/header/index.tsx
@@ -1,16 +1,19 @@
 import { useTheme } from '@/app/providers/ThemeContext';
 import { ThemeSwitcher } from '../shadcn-io/theme-switcher';
+import Link from 'next/link';
 
 export const Header = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <header className=" z-20 bg-background w-container fixed top-0 right-0 left-0 w-full border-b-2 border-gray-200 p-1 dark:border-b dark:border-gray-700">
+    <header className="bg-background w-container fixed top-0 right-0 left-0 z-20 w-full border-b-2 border-gray-200 p-1 dark:border-b dark:border-gray-700">
       <div className="m-auto flex w-5/6 items-center justify-between">
-        <h1 className="bold text-3xl">
-          Simula
-          <span className="ai bold text-5xl text-pink-500 italic">.aí</span>
-        </h1>
+        <Link href="/">
+          <h1 className="bold text-3xl">
+            Simula
+            <span className="ai bold text-5xl text-pink-500 italic">.aí</span>
+          </h1>
+        </Link>
         <ThemeSwitcher value={theme} onChange={(e) => toggleTheme(e)} />
       </div>
     </header>
